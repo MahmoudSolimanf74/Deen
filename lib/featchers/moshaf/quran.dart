6aff251fd640a72d0e8daa03c5edf0045844e5a2
MahmoudSliman74 <mahmoudsolimanf74@gmail.com>
2026-03-05 13:56:21 +0200

    quran page

--- lib/featchers/moshaf/quran.dart
+++ lib/featchers/moshaf/quran.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:quran_library/quran.dart';
 
 class Quran extends StatelessWidget {
   const Quran({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return QuranLibraryScreen(parentContext: context);
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:eslamicapp/featchers/onboard/splach_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:quran_library/quran.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await GetStorage.init();
+  await QuranLibrary.init();
+  QuranLibrary.initWordAudio();
   runApp(const MyApp());
 }
 
@@ -11,6 +17,6 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: SplachScreen());
+    return MaterialApp(debugShowCheckedModeBanner: false, home: SplachScreen());
   }
 }
